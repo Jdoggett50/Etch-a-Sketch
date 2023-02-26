0c7187cb1ecc4fb43d6531857ac7b939d39aa3aa
Jdoggett50 <dillondoggett5@gmail.com>
2023-02-26 00:02:55 -0500

    sliderInput displays boxes instead of prompt

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-//store the input from form
-let userInput = prompt('what is your number?');
-let squared = Math.pow(userInput,2)
-let sqrt = Math.sqrt(squared)
-console.log(squared)
-console.log(sqrt)
+//create grid pattern
+const boxCount = document.querySelector('#box-count');
+const sliderInput = boxCount.value;
+let squared = Math.pow(sliderInput,2);
+let sqrt = Math.sqrt(squared);
 
-
-//# of rows and columns
 function setGridLines(){
     const container = document.querySelector('#container');
     container.style.gridTemplateRows = `repeat(${sqrt}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${sqrt}, 1fr)`;
 } setGridLines()
 
-
-function getNumberOfSquares(){
+function generateSquares(){
     for(let i = 0; i < squared; i++){ 
     const container = document.querySelector('#container');
     const div = document.createElement('div');
-    div.style.border ='1px solid'
-    container.append(div)
+    div.style.border ='1px solid';
+    container.append(div);
     }
-}   getNumberOfSquares()
+} generateSquares()
+
