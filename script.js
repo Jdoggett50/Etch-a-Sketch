7dd0debe872e37c223fd266374802abde9f6a228
Jdoggett50 <dillondoggett5@gmail.com>
2023-02-25 22:47:38 -0500

    Implemented grid display

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-let numberOfSquares = 4;
+//store the input from form
+let userInput = prompt('what is your number?');
+let squared = Math.pow(userInput,2)
+let sqrt = Math.sqrt(squared)
+console.log(squared)
+console.log(sqrt)
+
+
+//# of rows and columns
+function setGridLines(){
+    const container = document.querySelector('#container');
+    container.style.gridTemplateRows = `repeat(${sqrt}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${sqrt}, 1fr)`;
+} setGridLines()
+
 
 function getNumberOfSquares(){
-    for(let i = 0; i < numberOfSquares; i++){
-    //if using grid-can indicate grid lines depending on 
+    for(let i = 0; i < squared; i++){ 
     const container = document.querySelector('#container');
     const div = document.createElement('div');
     div.style.border ='1px solid'
     container.append(div)
     }
 }   getNumberOfSquares()
-
-//determine placement
-const selectionSquared = () => {
-    return numberOfSquares * numberOfSquares
-}
-
-console.log(selectionSquared())
-
-//# of rows and columns 
-function setGridArea(){
-    const container = document.querySelector('#container');
-    container.style.gridTemplateRows = `repeat(2, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(2, 1fr)`;
-} setGridArea()
-
