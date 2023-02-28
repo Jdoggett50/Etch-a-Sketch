257747a230618ad1b8981c708e5a6c00646a4e87
Jdoggett50 <dillondoggett5@gmail.com>
2023-02-27 23:35:12 -0500

    squares and gridlines generate respective to slider input

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 //create grid pattern
-const boxCount = document.querySelector('#box-count');
-const sliderInput = boxCount.value;
-let squared = Math.pow(sliderInput,2);
-let sqrt = Math.sqrt(squared);
+const slider = document.querySelector('#slider');
 
 function setGridLines(){
+    let sliderSqrt = Math.sqrt(Math.pow(slider.value,2))
     const container = document.querySelector('#container');
-    container.style.gridTemplateRows = `repeat(${sqrt}, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(${sqrt}, 1fr)`;
-} setGridLines()
+    container.style.gridTemplateRows = `repeat(${sliderSqrt}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${sliderSqrt}, 1fr)`;
+}
 
 function generateSquares(){
-    for(let i = 0; i < squared; i++){ 
+    for(let i = 0; i < Math.pow(slider.value,2); i++){ 
     const container = document.querySelector('#container');
     const div = document.createElement('div');
     div.style.border ='1px solid';
     container.append(div);
     }
-} generateSquares()
+}
 
+slider.addEventListener ('input', () => {
+    setGridLines()
+    generateSquares()
+})
+
+//zero grid needs to start at 0
+//clear grid when new selection is made
