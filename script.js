ee510617d328667746be8f624a4731e9db030571
Jdoggett50 <dillondoggett5@gmail.com>
2023-03-02 22:26:36 -0500

    slider inserts correct area - reset button added

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 //create grid pattern
 const slider = document.querySelector('#slider');
+const container = document.querySelector('#container');
+const resetBtn = document.querySelector('#reset')
 
 function setGridLines(){
-    let sliderSqrt = Math.sqrt(Math.pow(slider.value,2))
-    const container = document.querySelector('#container');
-    container.style.gridTemplateRows = `repeat(${sliderSqrt}, 1fr)`;
+  let sliderSqrt = Math.sqrt(Math.pow(slider.value,2))
+  container.style.gridTemplateRows = `repeat(${sliderSqrt}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${sliderSqrt}, 1fr)`;
-}
+} 
 
-function generateSquares(){
-    for(let i = 0; i < Math.pow(slider.value,2); i++){ 
-    const container = document.querySelector('#container');
+function generateSquareArea(){
+    container.textContent ='';
+    for(let i = 0; i < Math.pow(slider.value, 2); i++){
     const div = document.createElement('div');
     div.style.border ='1px solid';
     container.append(div);
     }
 }
 
+function colorTarget(evt) {
+  evt.target.classList.add('colored-squares');
+}
+
+function resetGrid() {
+  console.log(container.classList)
+} 
+
 slider.addEventListener ('input', () => {
     setGridLines()
-    generateSquares()
+    generateSquareArea()
 })
 
-//zero grid needs to start at 0
-//clear grid when new selection is made
+resetBtn.addEventListener('click', () => {
+  setGridLines()
+  generateSquareArea()
+})
+
+container.addEventListener('mouseover', colorTarget)
