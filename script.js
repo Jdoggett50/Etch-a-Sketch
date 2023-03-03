e12d6903276e2c7fa7f4118ee2a2ba24edabd10d
Jdoggett50 <dillondoggett5@gmail.com>
2023-03-02 23:51:06 -0500

    added lines on/off btn - started functionality

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 //create grid pattern
 const slider = document.querySelector('#slider');
 const container = document.querySelector('#container');
-const resetBtn = document.querySelector('#reset')
+const resetBtn = document.querySelector('#reset-btn');
+const lineBtn = document.querySelector('#line-btn');
 
 function setGridLines(){
-  let sliderSqrt = Math.sqrt(Math.pow(slider.value,2))
-  container.style.gridTemplateRows = `repeat(${sliderSqrt}, 1fr)`;
+    let sliderSqrt = Math.sqrt(Math.pow(slider.value,2))
+    container.style.gridTemplateRows = `repeat(${sliderSqrt}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${sliderSqrt}, 1fr)`;
 } 
 
@@ -13,27 +14,30 @@ function generateSquareArea(){
     container.textContent ='';
     for(let i = 0; i < Math.pow(slider.value, 2); i++){
     const div = document.createElement('div');
-    div.style.border ='1px solid';
+    div.classList.add('toggle-lines')
     container.append(div);
     }
 }
 
 function colorTarget(evt) {
-  evt.target.classList.add('colored-squares');
+    evt.target.classList.add('colored-squares');
 }
-
-function resetGrid() {
-  console.log(container.classList)
-} 
 
 slider.addEventListener ('input', () => {
     setGridLines()
     generateSquareArea()
-})
+    console.log(innerDivLines)
+    }
+)
 
 resetBtn.addEventListener('click', () => {
-  setGridLines()
-  generateSquareArea()
-})
+    setGridLines()
+    generateSquareArea()
+    }
+)
 
 container.addEventListener('mouseover', colorTarget)
+
+lineBtn.addEventListener('click', () => {
+    innerDivLines.classList.remove('toggle-lines')
+})
