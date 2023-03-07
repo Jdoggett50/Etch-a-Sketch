604fa448c372ad9943ab2885bd4cd208989ee577
Jdoggett50 <dillondoggett5@gmail.com>
2023-03-06 22:03:24 -0500

    adjusted reset functionality - datalist selects box-fill functions

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const slider = document.querySelector('#slider');
 const container = document.querySelector('#container');
 const resetBtn = document.querySelector('#reset-btn');
 const lineBtn = document.querySelector('#line-btn');
-const colorBlack = document.querySelector('#color-black')
-const randomColor = document.querySelector('#random-color')
-let containerChildNodes = container.childNodes;
+let myFill = document.querySelector('#my-fill') 
 
 function setGridLines(){
-    container.style.backgroundColor = 'white';
     let sliderSqrt = Math.sqrt(Math.pow(slider.value,2))
+    container.style.backgroundColor ='white';
     container.style.gridTemplateRows = `repeat(${sliderSqrt}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${sliderSqrt}, 1fr)`;
 } 
@@ -20,40 +18,53 @@ function generateSquareArea(){
     container.append(div);
     }
 }
-slider.addEventListener ('input', () => {
+
+function fillBlack(evt) {
+    evt.target.style.backgroundColor ='black'
+}
+
+function toggleLines (){
+  // container.classList.toggle('lines')
+  let containerChildNodes = container.childNodes;
+  containerChildNodes.forEach((child) => {
+    child.classList.toggle('lines')
+    }
+)}
+
+function randomColor(evt){
+  let random = () => Math.floor(Math.random()*258);
+  evt.target.style.backgroundColor =`rgb(${random()},${random()},${random()})`
+}
+
+myFill.addEventListener('input',() => {
+  if(myFill.value.toLowerCase() == 'black')   {
+    container.removeEventListener('mouseover',randomColor);
+    container.addEventListener('mouseover', fillBlack);
+  } else if (myFill.value.toLowerCase() == 'random'){ 
+    container.removeEventListener('mouseover', fillBlack);
+    container.addEventListener('mouseover', randomColor);
+  } else if(myFill.value.toLowerCase() == 'none'){        
+    container.removeEventListener('mouseover',randomColor);
+    container.removeEventListener('mouseover',fillBlack);
+  }
+})
+
+lineBtn.addEventListener('click', () => {
+    toggleLines()
+    }
+  )
+
+  slider.addEventListener ('input', () => {
     setGridLines()
     generateSquareArea()
     }
 )
 
 resetBtn.addEventListener('click', () => {
+    container.removeEventListener('mouseover', fillBlack);
+    container.removeEventListener('mouseover',randomColor);
+    myFill.value = 'None';
     setGridLines()
     generateSquareArea()
     }
 )
-
-function fillBlack(evt) {
-    evt.target.classList.add('black-fill');
-}
-container.addEventListener('mouseover',fillBlack)
-
-function toggleLines (){
-containerChildNodes.forEach((child) => {
-        child.classList.toggle('lines')
-        }
-    )
-}
-
-lineBtn.addEventListener('click',toggleLines)
-
-function toRandomColor(evt){
-    let random = () => Math.floor(Math.random()*258);
-    evt.target.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`
-}
-
-//  Notes:
-//      random button activates the colors
-//      one should activate and the other register the 
-//      color change
-//  toggle the text inside the button
-//  create a button to generate random rgb values
